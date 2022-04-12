31075dfe5c389c1d41abd9bbf7bb334a21836696
marius <marius.franck@uphf.fr>
2022-04-12 18:09:32 +0200

    Derniere vesion sans deplacements joueurs

---
+++ Terraria/src/CodeV2/Main.java
@@ -0,0 +1,9 @@
+package CodeV2;
+
+public class Main {
+    public static void main(String[] args) {
+        Monde m = new Monde(1);
+        m.creer_monde();
+        m.afficher_console(1);
+    }
+}
